cb1f1dfaf0ae9e63f2414d2b8d99aa749f62ab6d
Erjan04 <theerjoinlast@gmail.com>
2022-10-07 14:40:59 +0300

    remove class AnimeGirlState

--- app/src/main/java/kg/erjan/composepractice/presentation/state/AnimeGirlsState.kt
+++
@@ -1,9 +0,0 @@
-package kg.erjan.composepractice.presentation.state
-
-import kg.erjan.composepractice.domain.models.anime_girl.AnimeGirl
-
-data class AnimeGirlsState(
-    val animeGirlInfo: AnimeGirl? = null,
-    val isLoading: Boolean = false,
-    val error: String? = null
-)
